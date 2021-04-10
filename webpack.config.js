2c178cc61f071de5490e90e282b9d0786f42f1cd
Jeremy Lavigne <lavigne.jeremy@post.com>
2021-04-10 18:29:36 +0200

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,16 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./build-utils/webpack.common");
 
-const port = process.env.PORT || 3000;
+const { merge } = require("webpack-merge");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "[name].[hash].js",
-    publicPath: "/",
-  },
-  resolve: {
-    alias: {
-      "react-dom": "@hot-loader/react-dom",
-    },
-  },
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              localsConvention: "camelCase",
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-      favicon: "public/favicon.ico",
-    }),
-  ],
-  devServer: {
-    host: "localhost",
-    port: port,
-    historyApiFallback: true,
-    open: true,
-    hot: true,
-  },
+module.exports = () => {
+  let envConfig;
+
+  if (process.env.NODE_ENV === "production") {
+    envConfig = require(`./build-utils/webpack.prod.js`);
+  } else {
+    envConfig = require(`./build-utils/webpack.dev.js`);
+  }
+  const mergedConfig = merge(commonConfig, envConfig);
+
+  return mergedConfig;
 };
