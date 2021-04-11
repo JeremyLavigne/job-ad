fc8b080304e61ee84dc40fe1cb1b4ba0c74ad500
Jeremy Lavigne <lavigne.jeremy@post.com>
2021-04-11 16:42:15 +0200

    Header + Icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { ImLocation2 } from "react-icons/im";
+import { MdShoppingCart } from "react-icons/md";
 
 const Header = () => {
   return (
     <header>
-      <div>I am a Header</div>
+      <div className="header-delivery-container">
+        <ImLocation2 className="header-delivery-icon" />
+        <span className="header-delivery-stockholm">Deliver to Stockholm</span>
+      </div>
+
+      <div className="header-searchbar-container">
+        <input />
+      </div>
+
+      <div className="header-flag-container">
+        <img src="https://www.countryflags.io/se/flat/32.png" />
+      </div>
+
+      <div className="header-cart-container">
+        <MdShoppingCart />
+        <span className="header-cart-number">0</span>
+      </div>
     </header>
   );
 };
