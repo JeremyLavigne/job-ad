665b66c2745ec49a1a110c49642b82bb25dd9ee5
Jeremy Lavigne <lavigne.jeremy@post.com>
2021-04-12 13:14:33 +0200

    shop section

--- src/components/pages/home/ShopSection.js
+++ src/components/pages/home/ShopSection.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { ImLocation2 } from "react-icons/im";
+
+const Button = ({ content }) => {
+  return <button className="shop-btn">{content}</button>;
+};
 
 const ShopSection = () => {
   return (
     <article className="shop-section">
-      <div>Price - SEK 299999</div>
-      <div>Arrives - May 1st</div>
-      <div>In Stock</div>
-      <div>
-        <button>Add to Cart</button>
-        <button>Hire Now</button>
-        <div>Deliver to Stockholm</div>
+      <div className="shop-section-top">
+        <div>
+          <span className="description-price">29999 kr/mth</span>
+          <span className="shop-fees">No shipping fees.</span>
+        </div>
+
+        <div>
+          Arrives: <span className="shop-arrives">Next Monday</span>
+        </div>
+
+        <div className="shop-stock">In Stock.</div>
+      </div>
+
+      <div className="shop-buttons-container">
+        <Button content="Add to Cart" />
+        <Button content="Hire Now" />
+      </div>
+
+      <div className="header-delivery-container">
+        <ImLocation2 className="header-delivery-icon" />
+        <span className="header-delivery-stockholm">Deliver to Stockholm</span>
       </div>
     </article>
   );
