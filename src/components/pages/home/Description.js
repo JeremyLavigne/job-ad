c13350c9d56646e28c93a57acd8f66cc6e13d1e6
Jeremy Lavigne <lavigne.jeremy@post.com>
2021-04-12 11:55:52 +0200

    Description content & style

--- src/components/pages/home/Description.js
+++ src/components/pages/home/Description.js
@@ -1,25 +1,55 @@
 import React from "react";
 
+const Choice = ({ content, isActive }) => {
+  return (
+    <div
+      className={`description-choice ${
+        isActive && "description-choice--active"
+      }`}
+    >
+      {content}
+    </div>
+  );
+};
+
 const Description = () => {
   return (
     <article className="description-section">
-      <div>
+      <div className="description-header">
         <h2>Web Developer</h2>
-        <span>&#9734;&#9734;&#9734;&#9734;&#9734;</span>
-        <span>1 rating</span>
+        <span className="description-rating-stars">
+          &#9733;&#9733;&#9733;&#9733;&#9733;
+        </span>
+        <span className="description-rating-label">1 rating</span>
       </div>
 
-      <div>
+      <div className="description-top">
         <div>
-          Price - <span>SEK 29999</span>
+          Price: <span className="description-price">SEK 29999</span>
         </div>
-        <div>Junior - Senior</div>
-        <div>Frontend - Fullstack</div>
-        <div>Full time - Part time</div>
+        <div className="description-choices-container">
+          <h5>Experience</h5>
+          <div className="description-choices">
+            <Choice content="Junior" isActive />
+            <Choice content="Senior" />
+          </div>
+
+          <h5>Position</h5>
+          <div className="description-choices">
+            <Choice content="Frontend" />
+            <Choice content="Fullstack" isActive />
+          </div>
+
+          <h5>Worktime</h5>
+          <div className="description-choices">
+            <Choice content="Full Time" isActive />
+            <Choice content="Part Time" />
+          </div>
+        </div>
       </div>
 
-      <div>
-        <h5>About</h5>
+      <div className="description-about">
+        <h4>About</h4>
         <ul>
           <li>Build : 2019</li>
           <li>blah blah</li>
