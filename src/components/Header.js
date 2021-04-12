7f2f652d5f921c9c3eaee14d6f836f4159b36e25
Jeremy Lavigne <lavigne.jeremy@post.com>
2021-04-12 16:32:02 +0200

    Header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { ImLocation2 } from "react-icons/im";
 import { MdShoppingCart } from "react-icons/md";
+import { BiSearchAlt } from "react-icons/bi";
 
 const Header = () => {
   return (
     <header>
       <div className="header-delivery-container">
-        <ImLocation2 className="header-delivery-icon" />
-        <span className="header-delivery-stockholm">Deliver to Stockholm</span>
+        <div>
+          <ImLocation2 className="header-delivery-icon" />
+        </div>
+        <div>
+          <div>Deliver to</div>
+          <div className="header-delivery-stockholm">Stockholm</div>
+        </div>
       </div>
 
-      <div className="header-searchbar-container">
-        <input />
-      </div>
+      <div className="header-middle">
+        <div className="header-searchbar-container">
+          <input className="header-searchbar-input" />
+          <div className="header-searchbar-icon-container">
+            <BiSearchAlt className="header-searchbar-icon" />
+          </div>
+        </div>
 
-      <div className="header-flag-container">
         <img src="https://www.countryflags.io/se/flat/32.png" />
       </div>
 
       <div className="header-cart-container">
-        <MdShoppingCart />
-        <span className="header-cart-number">0</span>
+        <MdShoppingCart className="header-cart-icon" />
+        <div className="header-cart-number">1</div>
       </div>
     </header>
   );
